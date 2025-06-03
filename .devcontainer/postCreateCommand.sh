1b409de4afc2046418e62fd8fda240ce0d510100
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-06-03 14:50:03 +0000

    update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,8 @@ set -e
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --android-studio-version) 
-            ANDROID_STUDIO_VERSION="$2"
+        --atak-sdk) 
+            ATAK_SDK="$2"
             shift
             ;;
         --basename) 
@@ -16,37 +16,61 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-unzip -q /tmp/ATAK-CIV-SDK.zip -d /tmp
-sudo cp -r /tmp/ATAK-CIV*-SDK/* /workspaces
-sudo rm -rf /tmp/ATAK-CIV-SDK.zip /tmp/ATAK-CIV*-SDK
+# Create default keystores
+for w in debug release
+do
+  if [ -f "/workspaces/${w}.keystore" ]; then
+    echo "${w}.keystore already exists in /workspaces, skipping generation."
+  else
+    echo "Generating ${w}.keystore..."
+    keytool -genkey -v -keystore ${w}.keystore -alias android${w}key \
+      -storepass android -keypass android -keyalg RSA -keysize 2048 -validity 10000 \
+      -dname "CN=TAK, OU=TAK, O=TAK, L=TAK, S=TAK, C=SE"
+    sudo cp ${w}.keystore /workspaces
+  fi
+done
 
-mkdir -p /workspaces/plugins
-ln -s "/workspaces/${BASENAME}" "/workspaces/plugins/${BASENAME}"
+# Apply SDK
+SDK_ZIP="/workspaces/${BASENAME}/.devcontainer/files/${ATAK_SDK}"
+SDK_PATH="/workspaces/${BASENAME}/.sdk"
 
-# for w in debug release
-# do
-#   if [ -f "/workspaces/${w}.keystore" ]; then
-#     echo "${w}.keystore already exists in /workspaces, skipping generation."
-#   else
-#     echo "Generating ${w}.keystore..."
-#     keytool -genkey -v -keystore ${w}.keystore -alias android${w}key \
-#       -storepass android  -keypass android -keyalg RSA -keysize 2048 -validity 10000 \
-#       -dname "CN=TAK, OU=TAK, O=TAK, L=TAK, S=TAK, C=SE"
-#     sudo cp ${w}.keystore /workspaces
-#   fi
-# done
+if [ ! -f "$SDK_ZIP" ]; then
+  echo "SDK zip not found at $SDK_ZIP."
+  echo "Rebuild the container."
+  exit 1
+fi
+
+if [ ! -f "$SDK_PATH/atak-gradle-takdev.jar" ]; then
+  echo "Extracting SDK..."
+  unzip -q $SDK_ZIP -d /tmp
+  mkdir -p $SDK_PATH
+  sudo cp -r /tmp/ATAK-CIV*-SDK/* $SDK_PATH
+  sudo rm -rf /tmp/ATAK-CIV-SDK.zip /tmp/ATAK-CIV*-SDK
+fi
 
 # Configure Android Studio application
-ANDROID_STUDIO_FOLDER="/home/vscode/.config/Google/AndroidStudio${ANDROID_STUDIO_VERSION}"
-mkdir -p "${ANDROID_STUDIO_FOLDER}"
-echo "ide.no.survey=true" >> "${ANDROID_STUDIO_FOLDER}/idea.properties"
+DATADIRECTORYNAME=$(jq -r '.dataDirectoryName' /opt/android-studio/product-info.json)
+ANDROID_STUDIO_FOLDER="/home/vscode/.config/Google/${DATADIRECTORYNAME}"
+mkdir -p "${ANDROID_STUDIO_FOLDER}/options"
 
-cat > "${ANDROID_STUDIO_FOLDER}/trusted-paths.json" <<EOF
-{
-  "trusted-paths": [
-    "/workspaces"
-  ]
-}
+cat > "${ANDROID_STUDIO_FOLDER}/options/trusted-paths.xml" <<EOF
+<application>
+  <component name="Trusted.Paths">
+    <option name="TRUSTED_PROJECT_PATHS">
+      <map>
+        <entry key="/workspaces/../${BASENAME}" value="true" />
+        <entry key="/workspaces/${BASENAME}" value="true" />
+      </map>
+    </option>
+  </component>
+  <component name="Trusted.Paths.Settings">
+    <option name="TRUSTED_PATHS">
+      <list>
+        <option value="/workspaces" />
+      </list>
+    </option>
+  </component>
+</application>
 EOF
 
 sudo chown -R vscode /workspaces
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# cp /workspaces/atak-javadoc.jar .
-
 # Run Android Studio application
 /opt/android-studio/bin/studio
